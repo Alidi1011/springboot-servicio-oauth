397d5bf8e2d0bc57bf9f8b785f8ede4cb8bd4b47
Alisson <arteaga10alisson@gmail.com>
2023-09-28 12:07:38 -0500

    add manange attempts in login

--- src/main/java/com/aarteaga/oauth/services/IUsuarioService.java
+++ src/main/java/com/aarteaga/oauth/services/IUsuarioService.java
@@ -4,4 +4,5 @@ import com.aarteaga.commons.usuarios.models.entity.Usuario;
 
 public interface IUsuarioService {
 	public Usuario findByUsername(String username);
+	public Usuario update(Usuario usuario, Long id);
 }
