7155fc8d093c32893965df1b0bb2a82c5bc0d70d
Alisson <arteaga10alisson@gmail.com>
2023-09-29 23:15:05 -0500

    update properties and encoded key

--- src/main/java/com/aarteaga/oauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/aarteaga/oauth/security/AuthorizationServerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -55,7 +56,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key"));
+		tokenConverter.setSigningKey(Base64.getEncoder().encodeToString(env.getProperty("config.security.oauth.jwt.key").getBytes()));
 		return tokenConverter;
 	}
 
